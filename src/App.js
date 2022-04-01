068064e7f0d2e21c33da5659bdabd4ef6db52252
avnishy <avnishy@alohatechnologydev.com>
2022-04-01 21:31:48 +0530

    Add Show Doc Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
 import AuthService from "./services/auth.service";
 import Login from "./components/login.component";
@@ -42,74 +43,86 @@ class App extends Component {
     const { currentUser, showReviewerBoard, showAdminBoard } = this.state;
     return (
       <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            DocSharing Platform
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/home"} className="nav-link">
-                Home
-              </Link>
-            </li>
-            {showReviewerBoard && (
-              <div className="navbar-nav ml-auto">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container  p-1">
+            <Link to={"/"} className="navbar-brand">
+              DocSharing Platform
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapsibleNavbar"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+              <div className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <Link to={"/rev"} className="nav-link">
-                    My Docs
+                  <Link to={"/home"} className="nav-link">
+                    Home
                   </Link>
                 </li>
-                <li className="nav-item">
-                  <Link to={"/addDocument"} className="nav-link">
-                    Add Document
-                  </Link>
-                </li>
+                {showReviewerBoard && (
+                  <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <Link to={"/rev"} className="nav-link">
+                        My Docs
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to={"/addDocument"} className="nav-link">
+                        Add Document
+                      </Link>
+                    </li>
+                  </div>
+                )}
+                {showAdminBoard && (
+                  <div className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <Link to={"/admin"} className="nav-link">
+                        Admin Board
+                      </Link>
+                    </li>
+                  </div>
+                )}
+                {currentUser && (
+                  <li className="nav-item">
+                    <Link to={"/user"} className="nav-link">
+                      Dashboard
+                    </Link>
+                  </li>
+                )}
               </div>
-            )}
-            {showAdminBoard && (
-              <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to={"/admin"} className="nav-link">
-                    Admin Board
-                  </Link>
-                </li>
-              </div>
-            )}
-            {currentUser && (
-              <li className="nav-item">
-                <Link to={"/user"} className="nav-link">
-                  Dashboard
-                </Link>
-              </li>
-            )}
+              {currentUser ? (
+                <div className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link to={"/profile"} className="nav-link">
+                      {currentUser.username}
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/login" className="nav-link" onClick={this.logOut}>
+                      LogOut
+                    </a>
+                  </li>
+                </div>
+              ) : (
+                <div className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link to={"/login"} className="nav-link">
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to={"/register"} className="nav-link">
+                      Signup
+                    </Link>
+                  </li>
+                </div>
+              )}
+            </div>
           </div>
-          {currentUser ? (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                  {currentUser.username}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
-                  LogOut
-                </a>
-              </li>
-            </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-              <Link to={"/register"} className="nav-link">
-                Signup
-              </Link>
-            </li>
-            </div>
-          )}
         </nav>
         <div className="container mt-3">
           <Switch>
