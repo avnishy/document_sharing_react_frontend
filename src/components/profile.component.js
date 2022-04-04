2121ad48bd5743a521e1110b1a8a88f59b914e70
avnishy <avnishy@alohatechnologydev.com>
2022-04-04 11:04:29 +0530

    Add Show Doc Page

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -9,18 +9,20 @@ export default class Profile extends Component {
   }
   render() {
     const { currentUser } = this.state;
+    const imagesrc = `https://robohash.org/${currentUser.username}.png?set=set2`
     return (
-      <div className="container ">
+      <div className="card card-container lead shadow p-10 mb-5 rounded ">
         <header className="jumbotron">
           <h3>
             <strong>{currentUser.username}</strong> Profile
           </h3>
         </header>
-        <p>
+        <img className="image--cover" style={{border: "6px", margin: "10px"}} src={imagesrc} alt="ProfilePic"/>
+        {/* <p>
           <strong>Token:</strong>{" "}
           {currentUser.accessToken.substring(0, 20)} ...{" "}
           {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
+        </p> */}
         <p>
           <strong>Id:</strong>{" "}
           {currentUser.id}
@@ -33,11 +35,11 @@ export default class Profile extends Component {
           <strong>Email:</strong>{" "}
           {currentUser.email}
         </p>
-        {/* <strong>Authorities:</strong>
+        <strong>Authorities:</strong>
         <ul>
           {currentUser.roles &&
             currentUser.roles.map((role, index) => <li key={index}>{role}</li>)} 
-        </ul> */}
+        </ul>
       </div>
     );
   }
